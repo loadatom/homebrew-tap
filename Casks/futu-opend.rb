3273eff92ac0de3d3dae3e6fe02afba24e146906
alexqli <alexqli@futunn.com>
2026-04-28 20:31:06 +0800

    fix filename

--- Casks/futu-opend.rb
+++ Casks/futu-opend.rb
@@ -1,4 +1,4 @@
-cask "FutuOpenD" do
+cask "futu-opend" do
     version "10.4.6409"
     sha256 "b483156f659558fd9941023f956e908e1e35139125385c723f4f34b77760f942" # SHA256 hash of the zip/dmg file
     url "https://github.com/loadatom/homebrew-tap/releases/download/v10.4.6409/FutuOpenD.zip"
