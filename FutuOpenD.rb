5bbd4ea0fef831dd68b30517b90b13afe58ed589
alexqli <alexqli@futunn.com>
2026-04-28 19:37:13 +0800

    init

---
+++ FutuOpenD.rb
@@ -0,0 +1,8 @@
+cask "FutuOpenD" do
+    version "10.4.6409"
+    sha256 "b483156f659558fd9941023f956e908e1e35139125385c723f4f34b77760f942" # SHA256 hash of the zip/dmg file
+    url "https://github.com/loadatom/homebrew-tap/releases/download/v10.4.6409/FutuOpenD.zip"
+    name "FutuOpenD financial app"
+    homepage "https://openapi.futunn.com"
+    app "FutuOpenD.app"
+  end
